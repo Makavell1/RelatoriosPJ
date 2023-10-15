390e5b57f99d5c9a06aa6546e0e6cc42d14c1bf8
Patrick Pereira <129559852+Makavell1@users.noreply.github.com>
2023-10-15 18:37:40 -0300

    Add files via upload

---
+++ telaRelatorios/src/main/java/com/mycompany/telarelatorios/TelaRelatorios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.telarelatorios;
+
+/**
+ *
+ * @author Makavelly
+ */
+public class TelaRelatorios {
+
+    public static void main(String[] args) {
+        
+    }
+}
